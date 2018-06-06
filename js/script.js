3885eee1a2d749a17b3fc741dbce27a3a32bcf11
aindoria <indoriaabhishek@gmail.com>
2018-06-06 15:56:56 -0700

    Add Basic structure

---
+++ js/script.js
@@ -0,0 +1,23 @@
+if (!navigator.geolocation){
+    alert("Unsupported browser. Please use a newer browser. We recommend Firefox.");
+}
+
+function getCurrentLocation(){
+
+}
+
+function saveCookieLocation(){
+    
+}
+
+function getCookieLocation(){
+
+}
+
+function compareLocation(){
+
+}
+
+function displayResults(){
+    
+}
