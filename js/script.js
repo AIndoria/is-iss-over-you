ca0c2ffead2f7221ba855c19b279e5b668e9f625
aindoria <indoriaabhishek@gmail.com>
2018-06-06 19:42:31 -0700

    Get ISS Location and Current User Location

--- js/script.js
+++ js/script.js
@@ -2,22 +2,43 @@ if (!navigator.geolocation){
     alert("Unsupported browser. Please use a newer browser. We recommend Firefox.");
 }
 
-function getCurrentLocation(){
+let did_the_chinese_blow_it_up=0;//Get the wikipedia 'The International Space Station *IS*' string and check for is/was. 
 
+function getCurrentLocation(callback){
+    navigator.geolocation.getCurrentPosition(function (position){
+        lat=position.coords.latitude;
+        lon=position.coords.longitude;
+        callback();
+    });
 }
 
-function saveCookieLocation(){
-    
+function getISSLocation(){
+  console.log("Fetching ISS location...");
+  var request=new XMLHttpRequest();
+  request.open("GET","http://api.open-notify.org/iss-now.json");
+  request.onload=function getRequest(){
+    var ISSData=JSON.parse(request.responseText);
+    console.log(ISSData);
+  }
+  request.send();
 }
 
-function getCookieLocation(){
+getCurrentLocation(getISSLocation);
 
-}
 
 function compareLocation(){
 
 }
 
 function displayResults(){
+
+}
+
+//TODO
+function saveCookieLocation(){
     
+}
+
+function getCookieLocation(){
+
 }
